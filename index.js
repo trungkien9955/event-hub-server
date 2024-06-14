d8be37f87a4f39ba38d4f3f4ece0d49dddec82bc
trungkien9955 <144137300+trungkien9955@users.noreply.github.com>
2024-06-14 20:31:48 +0700

    sign up screen

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const authRouter = require('./routers/authRouter')
 const app = express()
 app.use(cors())
+app.use(express.json())
 const PORT = 3001
-app.get('/auth/hello', (_req, res)=>{
-    res.send('<h1>Welcome to Event hub server!</h1>')
-})
+// app.get('/auth/hello', (_req, res)=>{
+//     res.send('<h1>Welcome to Event hub server!</h1>')
+// })
+app.use('/auth', authRouter)
 app.listen(PORT, error=>{
     if (error) {
         console.log(error)
---
+++ routers/authRouter.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+const authRouter = Router()
+authRouter.get('/hello', (_req, res)=>{
+    res.send("Hello world!")
+})
+authRouter.post('/register', (req, res)=> {
+    console.log(req.body)
+   res.send("resgister")
+})
+module.exports = authRouter
